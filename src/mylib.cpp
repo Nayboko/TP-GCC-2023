cd0c63f32d6514dfa5067b98f8af147ee1034814
Nayboko <cedric.pluta@laposte.net>
2023-04-11 20:23:47 +0200

    ✨feat: Implémentation de l'exercice 1

--- src/mylib.cpp
+++ src/mylib.cpp
@@ -13,7 +13,33 @@ int randomInt(int min, int max)
 
 void formeCreuse(int x, int y)
 {
+    std::string str = "";
 
+    if(x <= 0 || y <= 0)
+    {
+        std::cerr << "Attention, le nombre de colonne ou de ligne doit être strictement positif !" << std::endl;
+        return;
+    }
+
+    // Pour chaque colonne
+    for(int i = 0; i < x; ++i)
+    {   
+        // Pour chaque ligne
+        for (int j = 0; j < y; ++j)
+        {
+            // Si la colonne ou la ligne est la première ou la dernière, on affiche le caractère
+            if((i == 0 || i == x - 1) || (j == 0 || j == y - 1)){
+                str += "*";
+            }else{
+                // Sinon un espace vide
+                str+= " ";
+            }
+        }
+        // On s'assure qu'à la fin de la ligne, on fasse bien un retour à la ligne
+        str += "\n";
+    }
+
+    std::cout << str << std::endl;
 }
 
 float distanceManhattan(Point2D p1, Point2D p2)

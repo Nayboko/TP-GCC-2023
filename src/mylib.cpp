cdf342de6f3766cf1a0d01519eff5818b70f0476
Nayboko <cedric.pluta@laposte.net>
2023-04-11 20:24:44 +0200

    ✨ feat: Implémentation de l'exercice 2

--- src/mylib.cpp
+++ src/mylib.cpp
@@ -44,18 +44,51 @@ void formeCreuse(int x, int y)
 
 float distanceManhattan(Point2D p1, Point2D p2)
 {
-    return 0.0f;
+    return std::abs(p2.x - p1.x) + std::abs(p2.y - p1.y);
 }
 
 
 float distanceEuclidienne(Point2D p1, Point2D p2)
 {
-    return 0.0f;
+    return std::sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y) * (p2.y - p1.y)));
+}
+
+bool areEquals(const Point2D& A, const Point2D& B)
+{
+    return A.x == B.x && A.y == B.y && A.label == B.label; 
 }
 
 void plusProcheVoisin(std::vector<Point2D> points, Point2D P, int dist)
 {
+    std::vector<Point2D> ppv;
+    //Pour chaque point dans la liste des points (c'est comme ça qu'on fait un 'foreach' le plus souvent en C++)
+    for(Point2D point : points)
+    {
+        // On exclut le point P de la liste des point à visiter
+        if(areEquals(P,)) continue;
+        if(distanceEuclidienne(P, point) <= dist)
+        {
+            // On rajoute point dans la liste des Plus Proche Voisins (ppv)
+            ppv.push_back(point);
+        }
+    }
 
+    // On vérifie que la liste des plus proches voisins n'est pas vide
+    if(ppv.size() > 0)
+    {   
+        std::cout << "Liste des voisins les plus proches de " << P << " :" << std::endl;
+        for(Point2D point : ppv)
+        {
+            // Grace à la surcharge opérateur de << (Point2D operator<<(const Point2D& P)), on peut afficher directement le contenu de notre objet point
+            dist = distanceEuclidienne(P, point);
+            std::cout << "\t" << point << " dist : " << dist << std::endl;
+        }
+        std::cout << std::endl;
+    }
+    else
+    {
+        std::cout << "Aucun point n'est suffisament proche d'une distance de " << dist << " pour être considéré comme un voisin de " << P << std::endl;
+    }
 }
 
 void plusOuMoins()
